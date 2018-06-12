c60f5bc66fda2cd576fc41f9180bc9ff93cd8ea0
Praveen Narayanan <pravnar@indiana.edu>
2018-06-12 17:25:41 -0400

    Document what works and doesn't work re:reversible-jump

--- Tests.hs
+++ Tests.hs
@@ -30,9 +30,9 @@ check :: (Sing a, Sing b) => Term ('HMeasure ('HPair a b)) -> Base a -> IO ()
 check prog base = putStrLn $ format (title prog base t) (disintegrate prog base t)
     where t = Var obs
 
-constraintsOn :: (Sing a, Sing b, Inferrable a) => Term ('HMeasure ('HPair a b)) -> IO ()
-constraintsOn prog = print (fmap snd $ disintegrate prog b t)
-    where (b,t) = (fst (base 0) [], Var obs)
+constraintsOn :: (Sing a, Sing b, Inferrable a) => Term ('HMeasure ('HPair a b)) -> Term a -> IO ()
+constraintsOn prog t = print (fmap snd $ disintegrate prog b t)
+    where b = fst (base 0) []
 
 infer :: (Sing a, Sing b, Inferrable a) => Term ('HMeasure ('HPair a b)) -> Term a -> Trace (Term ('HMeasure b), Base a)
 infer prog t = let b      = fst (base 0) []
@@ -308,29 +308,89 @@ test3 = do_ [ x  :<~ stdNormal
             (Dirac (Pair (Pair (Var x) (Var y)) Unit))
     where (x,x',y) = (V "x", V "x'", V "y")
 
-revJumpProposal :: Model ('HPair ('HEither 'HReal ('HPair 'HReal 'HReal))
+
+-- | Reversible jump MCMC
+--------------------------------------------------------------------------------
+
+-- What works (June 12 2018):
+
+-- greensRatio revJumpTarget revJumpProposal
+-- greensRatio dimMatchTarget revJumpProposal
+
+-- What doesn't work (June 12 2018):
+-- greensRatio dimMatchTarget dimMatchProposal
+                     
+
+-- An example measure that we might find in the denominator of acceptance ratio
+-- That is, an example of a bindx target proposal for some target and some proposal
+revJumpDenom :: Model ('HPair ('HEither 'HReal ('HPair 'HReal 'HReal))
                                  ('HEither 'HReal ('HPair 'HReal 'HReal)))
                          'HUnit
-revJumpProposal = MPlus (do_ [ a  :<~ stdNormal
-                             , b  :<~ stdNormal
-                             -- , a' :<~ Dirac (Var a :: Term 'HReal) 
-                             , a' :<~ Normal (Var a) (Real 0.1)
-                             ]
-                             (Dirac (Pair (Pair (Inr (Pair (Var a) (Var b)))
-                                                (Inl (Var a')))
-                                          Unit)))
-                        (do_ [ x  :<~ stdNormal
-                             , x' :<~ Normal (Var x) (Real 0.1)
-                             , y  :<~ Normal (Var x) (Real 0.1) ]
-                             (Dirac (Pair (Pair (Inl (Var x))
-                                                (Inr (Pair (Var x') (Var y))))
-                                          Unit)))
+revJumpDenom = MPlus (do_ [ a  :<~ stdNormal
+                          , b  :<~ stdNormal
+                          -- , a' :<~ Dirac (Var a :: Term 'HReal) 
+                          , a' :<~ Normal (Var a) (Real 0.1)
+                          ]
+                          (Dirac (Pair (Pair (Inr (Pair (Var a) (Var b)))
+                                             (Inl (Var a')))
+                                       Unit)))
+                      (do_ [ x  :<~ stdNormal
+                           , x' :<~ Normal (Var x) (Real 0.1)
+                           , y  :<~ Normal (Var x) (Real 0.1) ]
+                           (Dirac (Pair (Pair (Inl (Var x))
+                                              (Inr (Pair (Var x') (Var y))))
+                                        Unit)))
     where (a,b,a',x,x',y) = (V "a", V "b", V "a'", V "x", V "x'", V "y")
 
 revJumpBase :: Base ('HPair ('HEither 'HReal ('HPair 'HReal 'HReal))
                             ('HEither 'HReal ('HPair 'HReal 'HReal)))
 revJumpBase = Bindx (Either Lebesgue_ (Bindx Lebesgue_ (const Lebesgue_))) $
               \x -> (Either Lebesgue_ (Bindx Lebesgue_ (const Lebesgue_)))
+
+-- This works currently (June 12 2018)
+-- let rswap = evalNames $ liftMeasure Fst revJumpNumer >>= \p -> liftMeasure switch p >>= \p' -> pairWithUnit p'
+-- allBases rswap (Var obs)
+
+type R2 = 'HPair 'HReal 'HReal
+type RPlusR2 = 'HEither 'HReal R2
+
+-- | This is the same as calling bindx revJumpTarget revJumpProposal
+-- (minimum working example)
+revJumpMWE :: Term ('HMeasure ('HPair RPlusR2 RPlusR2))
+revJumpMWE = Do (init :<~ MPlus (Do (a :<~ stdNormal) (Dirac (Inl (Var a :: Term 'HReal))))
+                                (do_ [ b :<~ stdNormal
+                                     , c :<~ stdNormal ]
+                                     (Dirac (Inr (Pair (Var b :: Term 'HReal) (Var c :: Term 'HReal))))))
+                       -- either_ (Real 5) (Pair (Real 42) (Real 43)))
+                (MPlus (do_ [ LetInl x (Var init :: Term RPlusR2)
+                            , x1 :<~ Normal (Var x) (Real 0.1)
+                            , x2 :<~ Normal (Var x) (Real 0.1) ]
+                            (Dirac (Pair (Var init) (Inr (Pair (Var x1) (Var x2))))))
+                       (do_ [ LetInr xy (Var init :: Term RPlusR2)
+                            , xy' :<~ Normal (Add (frst (Var xy :: Term R2))
+                                                  (scnd (Var xy :: Term R2)))
+                                             (Real 0.1) ]
+                            (Dirac (Pair (Var init) (Inl (Var xy'))))))
+    where (init,x,x1,x2,xy,xy',a,b,c) = (V "init", V "x", V "x1", V "x2", V "xy", V "xy'", V "a", V "b", V "c")
+
+revJumpTarget :: Term ('HMeasure RPlusR2)
+revJumpTarget = MPlus (Do (a :<~ stdNormal) (Dirac (Inl (Var a :: Term 'HReal))))
+                          (do_ [ b :<~ stdNormal
+                               , c :<~ stdNormal ]
+                               (Dirac (Inr (Pair (Var b :: Term 'HReal) (Var c :: Term 'HReal)))))
+    where (a,b,c) = (V "a", V "b", V "c")
+
+revJumpProposal :: Term RPlusR2 -> Term ('HMeasure RPlusR2)
+revJumpProposal e = MPlus (do_ [ LetInl x e
+                               , x1 :<~ Normal (Var x) (Real 0.1)
+                               , x2 :<~ Normal (Var x) (Real 0.1) ]
+                               (Dirac  (Inr (Pair (Var x1) (Var x2)))))
+                          (do_ [ LetInr xy e
+                               , xy' :<~ Normal (Add (frst (Var xy :: Term R2))
+                                                     (scnd (Var xy :: Term R2)))
+                                                (Real 0.1) ]
+                               (Dirac (Inl (Var xy'))))
+    where (x,x1,x2,xy,xy') = (V "x", V "x1", V "x2", V "xy", V "xy'")
 
 either_ :: (Sing a, Sing b) => Term a -> Term b -> Term ('HMeasure ('HEither a b))
 either_ a b = MPlus (Dirac (Inl a)) (Dirac (Inr b))
@@ -345,8 +405,20 @@ dimensionMatch = do_ [ x :<~ stdNormal
                                   (Var z)))
     where (x,y,z) = (V "xen", V "yen", V "zen")
 
+-- Based on Ken's idea: make the target have a density wrt the /usual/
+-- base measure, aka lebesgue on all leaves (eg. either lebesgue
+-- (bindx lebesgue (const lebesgue)))
+dimMatch :: Model 'HReal ('HEither 'HReal ('HPair 'HReal 'HReal))
+dimMatch = Do (init :<~ Normal (Real 0) (Real 10))
+              (MPlus (Do (y :<~ Normal (Var init) (Real 0.1))
+                         (Dirac (Pair (Var y) (Inl (Var init)))))
+                     (do_ [ next :<~ Normal (Var init) (Real 1)
+                          , y :<~ Normal (Var next) (Real 0.1) ]
+                          (Dirac (Pair (Var y) (Inr (Pair (Var init) (Var next)))))))
+    where (init, next, y) = (V "init", V "next", V "dimobs")
+
 dimMatchTarget :: Term ('HMeasure ('HEither 'HReal ('HPair 'HReal 'HReal)))
-dimMatchTarget = let p = choosePosterior (disintegrate dimensionMatch Lebesgue_) (Real 42)
+dimMatchTarget = let p = choosePosterior (disintegrate dimMatch Lebesgue_) (Real 42)
                  in case p of
                       Just t  -> t
                       Nothing -> error "dimMatchTarget: got nothing"
@@ -371,7 +443,9 @@ dimMatchProposal e = MPlus (do_ [ LetInl x e
 dim     = evalNames (bindx dimMatchTarget dimMatchProposal)
 dimrev  = evalNames (liftMeasure switch dim)
 udim    = evalNames (pairWithUnit dim)
-udimrev = evalNames (pairWithUnit dimrev)
+udimrev = evalNames (pairWithUnit dimrev)          
+
+-----------------------------------------------------------------------------------------------------
          
                      
 type NParamsType = 'HPair 'HReal 'HReal
